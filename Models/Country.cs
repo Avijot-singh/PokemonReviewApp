0eb6a035866f64928fd6a6ada46120b7fde204d3
Avijot-singh <singhavijot17@gmail.com>
2024-01-05 15:44:07 +1100

    One to many relationshipts

--- Models/Country.cs
+++ Models/Country.cs
@@ -4,5 +4,6 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Owner> Owner { get; set; } // A country has a collection of owners who belong to it 
     }
 }
